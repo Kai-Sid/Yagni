ab178453fba001f52479aa039d218282b87ad12f
labf402 <72875432@continental.edu.pe>
2024-09-10 15:50:35 -0500

    Refactorizacion de yagni

--- Prueba.py
+++ Prueba.py
@@ -1,1 +1,22 @@
+# file: test_operations.py
+import unittest
+from operations import X
+
+
+class TestOperations(unittest.TestCase):
+
+    def setUp(self):
+        self.calculadora = X()
+
+    def test_sumar(self):
+        self.assertEqual(self.calculadora.sumar(5, 3), 8)
+        self.assertEqual(self.calculadora.sumar(-1, 1), 0)
+
+    def test_restar(self):
+        self.assertEqual(self.calculadora.restar(10, 5), 5)
+        self.assertEqual(self.calculadora.restar(0, 5), -5)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
